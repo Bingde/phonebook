73a5e52373a2f183d19600b5a51e997df40d23e8
Bingde <yeslucky5@gmail.com>
2015-05-09 15:24:13 -0400

    ad phonebook
    ad phonebook

---
+++ application/pushbot.js
@@ -0,0 +1,40 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+		if(PushbotsPlugin.isiOS){
+	        PushbotsPlugin.initializeiOS("PUSHBOTS_APP_ID");
+            }
+      if(PushbotsPlugin.isAndroid){
+	PushbotsPlugin.initializeAndroid("5546a2e317795938478b458e", "433225875709");
+       }
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
